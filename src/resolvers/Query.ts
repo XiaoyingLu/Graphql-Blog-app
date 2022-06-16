55de2057b0fb195b3499f6d16a73dca0ed39b5ec
Amber Lu <amber.lu@ubisoft.com>
2022-06-16 15:10:26 -0300

    relational queries

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,8 +1,30 @@
 import { Context } from "..";
 
 export const Query = {
+  me: async (_: any, __: any, { prisma, userInfo }: Context) => {
+    if (!userInfo) return null;
+    return await prisma.user.findUnique({
+      where: {
+        id: userInfo.userId,
+      },
+    });
+  },
+  profile: async (
+    _: any,
+    { userId }: { userId: string },
+    { prisma }: Context
+  ) => {
+    return await prisma.profile.findUnique({
+      where: {
+        id: Number(userId),
+      },
+    });
+  },
   posts: async (_: any, __: any, { prisma }: Context) => {
     const posts = await prisma.post.findMany({
+      where: {
+        published: true,
+      },
       orderBy: [
         {
           createdAt: "desc",
--- src/schema.ts
+++ src/schema.ts
@@ -2,6 +2,8 @@ import { gql } from "apollo-server";
 
 export const typeDefs = gql`
   type Query {
+    me: User
+    profile(userId: ID!): Profile!
     posts: [Post!]!
   }
 
